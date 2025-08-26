5a2b88ec2d6622dd198309c03c38f54364e409c8
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2025-08-26 11:00:05 +0900

    Section3-23

---
+++ Section3/Section3-23.sql
@@ -0,0 +1,31 @@
+USE my_db;
+
+SELECT DATABASE();
+
+CREATE TABLE users(
+ id INT,
+ name VARCHAR(10),
+ age INT,
+ phone_number CHAR(13),
+ message TEXT
+)
+
+## テーブル一覧表示
+SHOW TABLES;
+
+# テーブルの定義確認
+DESCRIBE users;
+
+# テーブルの削除
+DROP TABLE users;
+
+SHOW TABLES;
+
+# テーブル作成
+CREATE TABLE users(
+ id INT PRIMARY KEY,
+ name VARCHAR(10),
+ age INT,
+ phone_number CHAR(13),
+ message TEXT
+)
