5d2b7c7569fbe443fe84af18f140da2d710f875d
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2025-09-07 10:41:11 +0900

    Section5-31

---
+++ Section5/Section5-31.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE day_4_9_db;
+
+SHOW DATABASES;
+
+USE day_4_9_db;
+
+SHOW TABLES;
+
+# usersの定義
+DESCRIBE users;
+
+SELECT * FROM users LIMIT 10;
+
+# =で絞り込む
+SELECT * FROM users WHERE name = "奥村 成美";
+
+SELECT * FROM users WHERE birth_place = "日本";
+
+# FROM -> WHERE -> ORDER BY -> LIMITの順で実行される
+SELECT * FROM users WHERE birth_place <> "日本" ORDER BY age LIMIT 10;
+
+SELECT * FROM users WHERE age>=50 LIMIT 10;
+
+# 日付の取り出し
+SELECT * FROM users WHERE birth_day < "2011-04-03";
+
+# UPDATE
+UPDATE users SET name = "奥山 成美" WHERE id=1;
+
+SELECT * FROM users WHERE id=1;
+
+# DELETE
+SELECT * FROM users ORDER BY id DESC LIMIT 1;
+
+DELETE FROM users WHERE id>190;
