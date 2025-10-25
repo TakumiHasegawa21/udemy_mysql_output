676db73e0fe8ea538b0fe03c5721a8f1ddb83f37
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2025-10-25 16:29:27 +0900

    Section8-50

---
+++ Section8/Section8-50.sql
@@ -0,0 +1,32 @@
+SHOW DATABASES;
+
+USE day_4_9_db;
+
+select * from users;
+
+start transaction;
+
+select * from customers;
+
+-- 主キーでUPDATE(行ロック)
+update customers set age=43 where id=1;
+
+-- 行ロック解除
+rollback;
+
+start transaction;
+
+-- 主キーでないカラムでアップデートをかけるとテーブル全体のロックがかかるので注意
+update customers set age=42 where name="河野 文典";
+
+-- SELECTのロック
+-- FOR SHARE(共有ロック)
+-- FOR UPDATE(排他ロック)
+
+start transaction;
+select * from customers where id=1 for share;
+rollback
+
+start transaction;
+select * from customers where id=1 for update;
+rollback;
