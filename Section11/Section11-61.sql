7b9dcef1e8eb065d8b2675cc980a0b3f1d955317
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2025-11-03 19:35:52 +0900

    Section11-62

--- Section11/Section11-61.sql
+++ Section11/Section11-61.sql
@@ -4,10 +4,64 @@ create database day_10_14_db;
 
 use day_10_14_db;
 
-show tables;
+select * from employees;
 
--- テーブルに別名をつける
+-- 部署一覧
+select * from departments;
+
+-- INで絞り込む
+select * from employees where department_id in(1,2);
+
+-- 副問合せを使う
+select * from departments where name in ("経営企画部", "営業部");
+
+select * from employees where department_id in
+	(select id from departments where name in ("経営企画部", "営業部")
+)
+
+select * from  students;
+select * from users;
+
+-- 複数カラムのIN (副問合せ)
+select * from  students
+where (first_name, last_name) in (
+	select first_name, last_name from users
+);
+
+-- 副問合せ3: 集計と使う
 select
-	cs.name
+	*
 from
-	classes as cs;
+	employees
+where
+	age = (select max(age) from employees);
+
+-- 副問合せ4: FROM句で用いる
+select
+	max(avg_age) as "部署ごとの平均年齢の最大"
+from
+	(select
+		department_id,
+		avg(age) as avg_age
+	from 
+		employees
+	group by
+		department_id
+	) as temp_emp;
+
+-- 副問合せ5: SELECTの中に書く
+select
+	cs.id,
+	cs.first_name,
+	cs.last_name,
+	(select
+		max(order_date)
+	from
+		orders as order_max
+	where
+		cs.id = order_max.customer_id
+	)
+from
+	customers cs
+where
+	cs.id < 10;
