0ab03fad23a818e767387b72409482d951cc7ce7
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2025-11-22 13:52:06 +0900

    Section14-74

---
+++ Section14/Section14-74.sql
@@ -0,0 +1,16 @@
+show databases;
+
+use day_10_14_db;
+
+show tables;
+
+select * from employees;
+
+-- WINDOW関数
+select *, AVG(age) OVER(), COUNT(*)OVER()
+from employees;
+
+-- PARTITION BY: 分割してその中で集計する
+select *, AVG(age) OVER(partition by department_id) as avg_age,
+count(*)over(partition by department_id) as count_department
+from employees;
