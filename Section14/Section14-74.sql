590ab35a1a1e937dc9cbd2537e596c56d6615856
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2025-11-22 22:01:24 +0900

    Section14-76

--- Section14/Section14-74.sql
+++ Section14/Section14-74.sql
@@ -21,3 +21,14 @@ select
 	count(*) over(order by age) as tmp_count
 from
 	employees;
+
+-- PARTITION BY + ORDER BY
+select *,
+count(*) over(partition by department_id order by age) as count_value
+from employees;
+
+-- 月毎の、合計をemployeesのIDで昇順に並び替えて出す
+select *, max(payment) over(partition by sa.paid_date order by emp.id)
+from employees as emp
+inner join salaries as sa
+on emp.id = sa.employee_id;
