e72faf35f6db4996d7c86d36ccb98b04ee7e1853
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2025-11-22 09:57:34 +0900

    Section12-65

--- Section12/Section12-64.sql
+++ Section12/Section12-64.sql
@@ -18,3 +18,13 @@ select * from employees as em
 where exists (
 select 1 from departments as dt where dt.name in ("営業部", "開発部") AND em.department_id = dt.id
 );
+
+select * from customers as ct
+where exists (select * from orders as od where ct.id = od.customer_id and od.order_date = "2020-12-31");
+
+-- NOT EXISTS(値が帰らなかった行を抽出)
+select * from customers as ct
+where exists (select * from orders as od where ct.id = od.customer_id and od.order_date = "2020-12-31");
+
+select * from employees as em1
+where exists (select 1 from employees em2 where em1.manager_id = em2.id);
