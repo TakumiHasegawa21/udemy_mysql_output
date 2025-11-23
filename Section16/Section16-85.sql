bf7233b44640cc9c4a692578b615fcd7060ffa04
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2025-11-23 16:17:47 +0900

    Section16-86

--- Section16/Section16-85.sql
+++ Section16/Section16-85.sql
@@ -39,3 +39,33 @@ employees as emp
 left join departments as dt
 on emp.department_id = dt.id
 set emp.department_name = coalesce(dt.name, "不明");
+
+-- WITHを使ったUPDATE
+select * from stores;
+
+alter table stores
+add all_sales int;
+
+select * from items;
+
+select * from orders;
+
+with tmp_sales as(
+	select it.store_id, sum(od.order_amount * od.order_price) as summary
+	from items as it
+	inner join orders as od
+	on it.id = od.item_id
+	group by it.store_id
+)
+update stores as st
+inner join tmp_sales as ts
+on st.id = ts.store_id
+set st.all_sales = ts.summary;
+
+-- DELETE
+delete from employees
+where department_id in(
+	select id from departments where name = "開発部"
+);
+
+select * from employees;
